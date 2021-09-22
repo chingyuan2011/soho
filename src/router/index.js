ebb8af145fe850887435ede25922b77d1e869a52
Ruei <ruei.wang@104.com.tw>
2021-09-23 00:04:10 +0800

    [功能] 新增 Contact  route 及 RWD 切版

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '@/pages/Index'
+import Contact from '@/pages/Contact'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'index',
     component: Index
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
   },
   {
     path: '*',
