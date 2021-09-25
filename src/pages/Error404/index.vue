e3478c1aafe6a7571ab3e2b8c43876fd695e137b
Ruei <ruei.wang@104.com.tw>
2021-09-25 15:37:51 +0800

    [樣式] error404 RWD

--- src/pages/Error404/index.vue
+++ src/pages/Error404/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="Error404">
-    Error404
+    <div class="container">
+      <div class="Error404__title">
+        您點選的網頁已經調整囉 ~ <br>歡迎逛逛我們新網頁
+      </div>
+      <div class="Error404__note futuraExtraBlack">
+        The website has encountered an error and cannot display the requested page.<br>
+        Please try again later.<br>
+        Sorry for the inconvenience.
+      </div>
+    </div>
+    <div class="footer__deco-brown">
+    </div>
   </div>
 </template>
 
