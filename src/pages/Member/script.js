fa5b300e5f0c4429408550ae23f27ffa1fcb8e92
Ruei <ruei.wang@104.com.tw>
2021-09-25 12:56:31 +0800

    [樣式] member page RWD

--- src/pages/Member/script.js
+++ src/pages/Member/script.js
@@ -1,7 +1,10 @@
 import {} from '@vue/composition-api'
-
+import Banner from '@/components/Banner'
 export default {
   name: 'Member',
+  components: {
+    Banner
+  },
   setup (props, context) {
   }
 }
