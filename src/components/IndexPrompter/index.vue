d50ac4dc93d0afb244bb6799c39b9c3d63adbeb6
Ryan <chingyuan2011@gmail.com>
2021-09-26 10:52:05 +0800

    [功能] 首頁跑馬燈

---
+++ src/components/IndexPrompter/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="IndexPrompter">
+    <div
+      id="IndexPrompter__text"
+      class="IndexPrompter__text"
+    >
+      宅享TASTy ! 安心Enjoy ! 即日起至8月31日，西堤推出全新輕套餐，來店自取全單85折哦！
+    </div>
+  </div>
+</template>
+
+<script>
+import {} from '@vue/composition-api'
+
+export default {
+  name: 'IndexPrompter',
+  setup (props, context) {
+  }
+}
+</script>
+
+<style lang="scss">
+.IndexPrompter {
+  display: flex;
+  align-items: center;
+  height: 48px;
+  overflow: hidden;
+  background-color: #951e22;
+
+  &__text {
+    position: relative;
+    padding: 0 5px;
+    font-size: 18px;
+    color: #fff;
+    white-space: nowrap;
+  }
+}
+
+// xxl | xl | lg | md | sm
+// @include screen (xl) {}
+</style>
