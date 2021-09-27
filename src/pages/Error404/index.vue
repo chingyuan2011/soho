0be39f4c1bcd7bab17fdc4acc0b373e8d6c9791b
Ruei <ruei.wang@104.com.tw>
2021-09-27 20:59:25 +0800

    [修復] contact 404 頁面樣式

--- src/pages/Error404/index.vue
+++ src/pages/Error404/index.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="Error404">
     <div class="container">
-      <div class="Error404__title">
-        您點選的網頁已經調整囉 ~ <br>歡迎逛逛我們新網頁
+      <div class="Error404__content">
+        <div class="Error404__title">
+          您點選的網頁已經調整囉 ~ <br>歡迎逛逛我們新網頁
+        </div>
+        <div class="Error404__note futuraExtraBlack">
+          The website has encountered an error and cannot display the requested page.<br>
+          Please try again later.<br>
+          Sorry for the inconvenience.
+        </div>
       </div>
-      <div class="Error404__note futuraExtraBlack">
-        The website has encountered an error and cannot display the requested page.<br>
-        Please try again later.<br>
-        Sorry for the inconvenience.
+      <div class="footer__deco-brown">
       </div>
-    </div>
-    <div class="footer__deco-brown">
     </div>
   </div>
 </template>
