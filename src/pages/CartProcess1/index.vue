c90abdcc55897fdf8133e1261f7b052de471190a
rueiwang <want2813@gmail.com>
2024-07-28 16:10:28 +0800

    feat: use title and breadcrumb

--- src/pages/CartProcess1/index.vue
+++ src/pages/CartProcess1/index.vue
@@ -5,16 +5,19 @@
         en="SHOPPING CART"
         zh="我的購物車"
       ></Title>
+      <Breadcrumb :link-data="[{title: 'MEMBER CENTER', path: '/cartProcess1.html'}]"></Breadcrumb>
     </div>
   </div>
 </template>
 
 <script>
 import Title from '@/components/Title'
+import Breadcrumb from '@/components/Breadcrumb'
 export default {
   name: 'CartProcess1',
   components: {
-    Title
+    Title,
+    Breadcrumb
   },
   data () {
     return {}
