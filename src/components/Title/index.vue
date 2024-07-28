efad856345528a16e2b8f68bb8794f7f93c9e74b
rueiwang <want2813@gmail.com>
2024-07-28 15:25:16 +0800

    feat: Title component

---
+++ src/components/Title/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="Title">
+    <div class="Title_wrap">
+      <div class="Title_en">
+        {{ en }}
+      </div>
+      <div class="Title_zh">
+        <span>{{ zh }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Title',
+  props: {
+    en: {
+      type: String,
+      required: true
+    },
+    zh: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props, context) {
+
+  }
+}
+</script>
+
+<style lang="scss">
+.Title {
+  display: flex;
+  justify-content: center;
+  font-size: 40px;
+  color: #002c18;
+  text-align: right;
+
+  &_wrap {
+  }
+
+  &_en,
+  &_zh {
+    margin-bottom: 0;
+    font-weight: 500;
+    line-height: 1.2;
+    letter-spacing: .3rem;
+  }
+
+  &_zh {
+    font-size: 36px;
+
+    span {
+      position: relative;
+
+      &::before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 0;
+        width: 3px;
+        height: 100%;
+        background-color: #002c18;
+        transform: translate(-500%, -50%) skew(35deg);
+        transform-origin: center;
+      }
+    }
+  }
+}
+// xxl | xl | lg | md | sm
+// @include screen (xl) {}
+</style>
