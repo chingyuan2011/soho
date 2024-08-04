9c653d967d1a76114dbabfb25e8b687fe7fe3ce6
rueiwang <want2813@gmail.com>
2024-08-04 15:24:59 +0800

    feat: init login and signup page

---
+++ src/pages/Login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Login"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+  <style lang="scss" src="./style.scss">
+
+  // xxl | xl | lg | md | sm
+  // @include screen (xl) {}
+  </style>
---
+++ src/pages/SignUp/indx.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="SignUp"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'SignUp',
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+    <style lang="scss" src="./style.scss">
+
+    // xxl | xl | lg | md | sm
+    // @include screen (xl) {}
+    </style>
