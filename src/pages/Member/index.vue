37f680294aea47a4af6b1b3b0495b463d1b644e0
rueiwang <want2813@gmail.com>
2024-08-11 18:46:04 +0800

    feat: member page RWD

--- src/pages/Member/index.vue
+++ src/pages/Member/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Member">
-    <div class="container container-page">
+    <div class="container-page">
       <div class="page_commonTitleArea">
         <Title
           en="MEMBER ACCOUNT"
@@ -10,184 +10,204 @@
           :link-data="[{ title: 'MEMBER ACCOUNT', path: '/Member.html' }]"
         ></Breadcrumb>
       </div>
+    </div>
 
-      <div class="Member_client">
-        <div class="Member_client-delivery">
-          <div class="Member_title">
-            會員資料 /
-          </div>
-          <div class="Member_client-form">
-            <div class="Member_client-form-item required">
-              <label for="name">姓名</label>
-              <input
-                id="name"
-                type="text"
-                name="name"
-                placeholder="請填入真實姓名"
+    <Tabs></Tabs>
+    <div class="container">
+      <div class="Member__wrap">
+        <div class="Member_avatar">
+          <div class="Member_avatar-pic">
+            <img
+              src="/soho/img/member/member_people.png"
+              alt=""
+            >
+            <button>
+              <img
+                src="/soho/img/member/member_edit.png"
+                alt=""
               >
-            </div>
-            <div class="Member_client-form-item required">
-              <label for="email">聯絡信箱</label>
-              <input
-                id="email"
-                type="text"
-                name="email"
-                placeholder="請填入電子信箱"
-              >
-            </div>
-            <div class="Member_client-form-item required">
-              <label for="phone">聯絡電話</label>
-              <input
-                id="phone"
-                type="text"
-                name="phone"
-                placeholder="請填入手機號碼"
-              >
-            </div>
-            <div class="Member_client-form-item">
-              <label for="note">訂單備註</label>
-              <textarea
-                id="note"
-                type="text"
-                name="note"
-                placeholder="請輸入備註留言訊息"
-              ></textarea>
-            </div>
-            <div class="Member_client-form-item">
-              <label
-                for="agree-read"
-                class="checkbox_wrap"
-              >
-                <input
-                  id="agree-read"
-                  type="checkbox"
-                  checked="checked"
-                >我已仔細閱讀並明瞭
-                「會員註冊條款」、「隱私權政策」、「訂購須知」所載內容及其意義茲同意該等條款規定，並願遵守網站現今、嗣後規範的各種規則。
-                <span class="checkmark"></span>
-              </label>
-            </div>
-            <div class="Member_client-form-item">
-              <label>是否索取公司統編發票</label>
-              <div class="Member_client-form-item-radio">
-                <label class="radio_wrap">
-                  <input
-                    name="ifNeedTaxNumber"
-                    type="radio"
-                  >
-                  <span class="checkmark">否</span>
-                </label>
-                <label class="radio_wrap">
-                  <input
-                    name="ifNeedTaxNumber"
-                    type="radio"
-                    checked="checked"
-                  >
-                  <span class="checkmark">是</span>
-                </label>
-              </div>
-            </div>
-            <div class="Member_client-form-item required">
-              <label for="companyName">公司抬頭</label>
-              <input
-                id="companyName"
-                type="text"
-                name="companyName"
-                placeholder="請填入公司抬頭"
-              >
-            </div>
-            <div class="Member_client-form-item required">
-              <label for="taxNumber">公司統編</label>
-              <input
-                id="taxNumber"
-                type="text"
-                name="taxNumber"
-                placeholder="請填入8位數字公司統編"
-              >
-            </div>
-          </div>
-        </div>
-        <div class="Member_client-payment">
-          <div class="Member_client-paymentOptions">
-            <div class="Member_title">
-              密碼 /
-            </div>
-            <button class="Member_client-paymentOption Member_client-payment-creditCard  active">
-              信用卡
-            </button><button class="Member_client-paymentOption Member_client-payment-ATM">
-              ATM匯款
             </button>
           </div>
-          <div class="Member_client-creditCard active">
-            <div class="Member_title">
-              社群帳號 /
+          <div class="Member_avatar-info">
+            <div class="Member_avatar-name">
+              吳小可
             </div>
-            <div class="Member_client-form">
-              <div class="Member_client-form-item required">
-                <label for="cardName">持卡人姓名</label>
-                <input
-                  id="cardName"
-                  type="text"
-                  name="cardName"
-                  placeholder="與信用卡上名稱相同"
-                >
-              </div>
-              <div class="Member_client-form-item required">
-                <label for="cartNumber">信用卡號碼</label>
-                <input
-                  id="cartNumber"
-                  type="text"
-                  name="cartNumber"
-                  placeholder="**** **** **** ****"
-                >
-              </div>
-              <div class="Member_client-form-item required">
-                <label for="expireDate">有效日期</label>
-                <input
-                  id="expireDate"
-                  type="text"
-                  name="expireDate"
-                  placeholder="MM/YY"
-                >
-              </div>
-              <div class="Member_client-form-item required">
-                <label for="cvc">安全碼（CVC）</label>
-                <input
-                  id="cvc"
-                  type="text"
-                  name="cvc"
-                  placeholder="後三碼"
-                >
-              </div>
-            </div>
-          </div>
-          <div class="Member_client-agreement">
-            <div class="Member_client-form-item">
-              <label
-                for="agree-privacy"
-                class="checkbox_wrap"
-              >
-                <input
-                  id="agree-privacy"
-                  type="checkbox"
-                  checked="checked"
-                >我同意網站<a href="javascript;'">服務條款</a>及<a
-                  href="javascript;'"
-                >隱私權政策</a>
-                <span class="checkmark"></span>
-              </label>
-              <span>您必須同意細則和私隱條款</span>
+            <div class="Member_avatar-type">
+              一般會員
             </div>
           </div>
         </div>
-      </div>
-      <div class="Member_footer">
-        <button
-          id="submit-client-form"
-          class="active"
-        >
-          更新
-        </button>
+        <div class="Member_info">
+          <div class="Member_info-data">
+            <div class="Member_title">
+              會員資料 /
+            </div>
+            <div class="Member_info-form">
+              <div class="Member_info-form-item required">
+                <label for="name">姓名</label>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  value="吳小可"
+                  placeholder="請填入真實姓名"
+                >
+              </div>
+              <div class="Member_info-form-item">
+                <label for="birthday">生日</label>
+                <input
+                  id="birthday"
+                  type="date"
+                  name="birthday"
+                  placeholder=""
+                >
+              </div>
+              <div class="Member_info-form-item">
+                <label>性別</label>
+                <div class="Member_info-form-item-radio">
+                  <label class="radio_wrap">
+                    <input
+                      name="gender"
+                      type="radio"
+                    >
+                    <span class="checkmark">男生</span>
+                  </label>
+                  <label class="radio_wrap">
+                    <input
+                      name="gender"
+                      type="radio"
+                      checked="checked"
+                    >
+                    <span class="checkmark">女生</span>
+                  </label>
+                </div>
+              </div>
+              <div class="Member_info-form-item required">
+                <label for="phone">聯絡電話</label>
+                <input
+                  id="phone"
+                  type="text"
+                  name="phone"
+                  value="0901138130"
+                  placeholder="請填入手機號碼"
+                >
+              </div>
+              <div class="Member_info-form-item required">
+                <label for="email">電子信箱</label>
+                <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  value="136designco.ltd@gmail.com"
+                  placeholder="請輸入電子信箱"
+                >
+              </div>
+
+              <div class="Member_info-form-item required ">
+                <label for="address">收件地址</label>
+                <div class="address-select-wrap">
+                  <div class="customSelect">
+                    <select
+
+                      address-select-wrap
+                      name="city"
+                    >
+                      <option
+                        value=""
+                        disabled
+                        selected
+                      >
+                        縣市
+                      </option>
+                    </select>
+                  </div>
+                  <div class="customSelect">
+                    <select name="area">
+                      <option
+                        value=""
+                        disabled
+                        selected
+                      >
+                        區域
+                      </option>
+                    </select>
+                  </div>
+                  <input
+                    id="code"
+                    type="text"
+                    name="code"
+                    value="000"
+                    placeholder="郵遞區號"
+                  >
+                </div>
+                <input
+                  id="address"
+                  type="text"
+                  name="address"
+                  placeholder="街道地址"
+                >
+              </div>
+            </div>
+          </div>
+          <div class="Member_info-password">
+            <div class="Member_info-passwordOptions">
+              <div class="Member_title">
+                密碼 /
+              </div>
+              <div class="Member_info-form-item required">
+                <label for="oldPwd">舊密碼</label>
+                <input
+                  id="oldPwd"
+                  type="password"
+                  name="oldPwd"
+                  placeholder="0901138130"
+                >
+              </div>
+              <div class="Member_info-form-item required">
+                <label for="newPwd">新密碼</label>
+                <input
+                  id="newPwd"
+                  type="password"
+                  name="newPwd"
+                  placeholder="136136136"
+                >
+              </div>
+              <div class="Member_info-form-item required">
+                <label for="confirmPwd">確認密碼</label>
+                <input
+                  id="confirmPwd"
+                  type="password"
+                  name="confirmPwd"
+                  placeholder="136136136"
+                >
+              </div>
+            </div>
+            <div class="Member_info-socialMedia active">
+              <div class="Member_title">
+                社群帳號 /
+              </div>
+              <div class="Member_line">
+                <div class="Member_line-pic">
+                  <img
+                    src="/soho/img/member/member_line.png"
+                    alt=""
+                  >
+                </div>
+                <button>
+                  解除綁定
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="Member_footer">
+          <button
+            id="submit-client-form"
+            class="active"
+          >
+            更 新
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -196,11 +216,15 @@
 <script>
 import Title from '@/components/Title'
 import Breadcrumb from '@/components/Breadcrumb'
+import Tabs from '@/components/Tabs'
+
 export default {
   name: 'Member',
   components: {
     Title,
-    Breadcrumb
+    Breadcrumb,
+    Tabs
+
   },
   data () {
     return {}
