6748d7d2684898b1b41cbfcff9753cbbab64d967
rueiwang <want2813@gmail.com>
2024-08-11 17:51:01 +0800

    feat: bonus page RWD

--- src/pages/Bonus/index.vue
+++ src/pages/Bonus/index.vue
@@ -18,7 +18,7 @@
           我的紅利點數 /
         </div>
         <div class="Bonus_total">
-          目前紅利點數為<span>100</span>點
+          目前紅利點數為 <span>100</span> 點
         </div>
         <div class="Bonus_table">
           <div class="Bonus_table-row Bonus_table-header">
@@ -44,45 +44,161 @@
           <div class="Bonus_table-body">
             <div class="Bonus_table-row">
               <div class="Bonus_table-cell">
-                <div class="product-pic">
-                  <img
-                    src="/soho/img/Bonus/Bonus_item1.png"
-                    alt=""
-                  >
+                <div class="text">
+                  2024/06/02
                 </div>
               </div>
               <div class="Bonus_table-cell">
-                <div class="product_name">
-                  六色海藻
-                </div>
-              </div>
-              <div class="Bonus_table-cell product_amount">
-                <div class="customSelect">
-                  <select>
-                    <option value="1">
-                      1
-                    </option>
-                    <option value="2">
-                      2
-                    </option>
-                  </select>
+                <div class="text">
+                  DW123456
                 </div>
               </div>
               <div class="Bonus_table-cell">
-                <div class="product_price">
-                  $330
+                <div class="text">
                 </div>
               </div>
               <div class="Bonus_table-cell">
-                <div class="product_status">
-                  <button>加入購物車</button>
-                  <button class="secondary">
-                    立即購買
-                  </button>
+                <div class="text">
+                  +30
                 </div>
               </div>
-              <div class="Bonus_table-cell product_delete ">
-                <button class=""></button>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  -50
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2025/12/31
+                </div>
+              </div>
+            </div>
+            <div class="Bonus_table-row">
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2024/05/29
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  HP234567
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  618年中慶健康禮盒
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  +120
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  -
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2025/12/31
+                </div>
+              </div>
+            </div>
+            <div class="Bonus_table-row">
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2024/05/02
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  –
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  感恩回饋
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  +30
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  -
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2025/12/31
+                </div>
+              </div>
+            </div>
+            <div class="Bonus_table-row">
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2024/05/17
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  -
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  紅利過期
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  -30
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  -
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2025/12/31
+                </div>
+              </div>
+            </div>
+            <div class="Bonus_table-row">
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2023/12/22
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  LO345678
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  新加入會員
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  +30
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  -
+                </div>
+              </div>
+              <div class="Bonus_table-cell">
+                <div class="text">
+                  2025/12/31
+                </div>
               </div>
             </div>
           </div>
