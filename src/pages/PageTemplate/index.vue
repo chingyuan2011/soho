df5dcd4322b280c5004b7e0f859f8a8c8ad28285
ryan.yang <ryan.yang@104.com.tw>
2024-08-14 22:24:29 +0800

    feat. pageTemplate

---
+++ src/pages/PageTemplate/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="PageTemplate">
+    <div class="container-page">
+      <div class="page_commonTitleArea">
+        <Title
+          en="RETURNS & REFUNDS"
+          zh="退換貨說明"
+        ></Title>
+        <Breadcrumb :link-data="[{title: 'RETURNS & REFUNDS', path: '/returnsAndRefunds.html'}]"></Breadcrumb>
+      </div>
+    </div>
+    <div class="container">
+      <h2 class="ReturnsAndRefunds_pageTitle">
+        退換貨說明 /
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import Title from '@/components/Title'
+import Breadcrumb from '@/components/Breadcrumb'
+export default {
+  name: 'Bonus',
+  components: {
+    Title,
+    Breadcrumb
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
